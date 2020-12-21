889bc6f8993f87852ed16ef4cdf743d734897f93
Yaroslav  Bolyukin <iam@lach.pw>
2020-12-21 21:44:01 +0300

    build!: update dependencies
    proc_macro_hygiene is no longer required as rust 1.45
    
    BREAKING CHANGE: instead of silently replacing unsupported characters
    with '?', now macro emits compilation error
    
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(proc_macro_hygiene)]
-
 extern crate proc_macro;
 
 use quote::quote;
@@ -44,7 +42,6 @@ use TransformType::{CString, CWString};
 /// Transforms passed string to needed form, used by proc macro at bottom
 fn transform(
     input: RealCString,
-    default_ch: char,
     transform_type: TransformType,
 ) -> proc_macro::TokenStream {
     let stream = match transform_type {
@@ -53,11 +50,14 @@ fn transform(
                 .string
                 .chars()
                 .to_owned()
-                .map(|cur_char| {
+                .enumerate()
+                .map(|(offset, cur_char)| {
                     let out: char = if cur_char as u32 <= transform_type.max_char() {
                         cur_char
                     } else {
-                        default_ch
+                        return quote! {
+                            compile_error!(concat!("Unsupported character \"", #cur_char, "\" at offset ", #offset)),
+                        };
                     };
                     match transform_type {
                         CString => {
@@ -88,8 +88,6 @@ fn transform(
 /// Used in vmprotect crate, because vmprotect disassembles code, and finds usages like this
 ///
 /// ```rust
-/// #![feature(proc_macro_hygiene)]
-///
 /// use real_c_string::real_c_string;
 /// assert_eq!(0i8, unsafe{*real_c_string!("")});
 ///
@@ -98,22 +96,11 @@ fn transform(
 /// for i in 0..13 {
 ///     assert_eq!(same_as_array_of_bytes[i], unsafe{*c_string.offset(i as isize)})
 /// }
-///
-/// let c_string = real_c_string!("Привет world!");
-/// // 63i8 == '?', russian characters are not fitting in 1 byte in utf-8 character set
-/// let same_as_array_of_bytes: [i8;14] = [63i8, 63i8, 63i8, 63i8, 63i8, 63i8, 32i8, 119i8, 111i8, 114i8, 108i8, 100i8, 33i8, 0i8];
-/// for i in 0..13 {
-///     assert_eq!(same_as_array_of_bytes[i], unsafe{*c_string.offset(i as isize)})
-/// }
 /// ```
 #[proc_macro]
 pub fn real_c_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    if input.is_empty() {
-        panic!("No passed tokens!");
-    }
     transform(
         parse_macro_input!(input as RealCString),
-        '?',
         TransformType::CString,
     )
 }
@@ -121,8 +108,6 @@ pub fn real_c_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
 /// Same as `real_c_string`, but used for wchar_t* strings
 ///
 /// ```rust
-/// #![feature(proc_macro_hygiene)]
-///
 /// use real_c_string::real_c_wstring;
 /// assert_eq!(0i16, unsafe{*real_c_wstring!("")});
 ///
@@ -140,12 +125,8 @@ pub fn real_c_string(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
 /// ```
 #[proc_macro]
 pub fn real_c_wstring(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    if input.is_empty() {
-        panic!("No passed tokens!");
-    }
     transform(
         parse_macro_input!(input as RealCString),
-        '?',
         TransformType::CWString,
     )
 }
